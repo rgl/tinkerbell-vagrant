c87dd6c19a17ebc6eeba88cd3160100913fb6960
Rui Lopes <rgl@ruilopes.com>
2020-06-10 18:35:07 +0100

    mention the use of eth0 instead of eth1 for getting the ip address in the host network

--- provision-tinkerbell.sh
+++ provision-tinkerbell.sh
@@ -59,6 +59,9 @@ done
 
 # show summary.
 # e.g. inet 192.168.121.160/24 brd 192.168.121.255 scope global dynamic eth0
+# NB this gets the IP of the vagrant management interface (eth0) because its
+#    the only one accessible from the host (where libvirt is running) when
+#    we are connecting eth1 to the linux bridge.
 host_ip_address="$(ip addr show eth0 | perl -n -e'/ inet (\d+(\.\d+)+)/ && print $1')"
 cat <<EOF
 
