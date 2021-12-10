f3fb206eb04021908187ae95792fd759e3faf0fb
Rui Lopes <rgl@ruilopes.com>
2021-12-10 08:55:50 +0000

    upgrade to tinkerbell tink b72ab0bd24a47ef3c42d682f9ab80e3825655a82

--- provision-tinkerbell-tink.sh
+++ provision-tinkerbell-tink.sh
@@ -7,12 +7,12 @@ if [ ! -d ~/tinkerbell-tink ]; then
     cd ~
     git clone --no-checkout https://github.com/tinkerbell/tink.git tinkerbell-tink
     cd tinkerbell-tink
-    git checkout -f 187a3c7b37caf17be5c17a612da752778b2940f7 # 2021-11-02T19:01:59Z
+    git checkout -f b72ab0bd24a47ef3c42d682f9ab80e3825655a82 # 2021-12-03T22:35:03Z
 fi
 
 # build tink-worker.
-# see https://github.com/tinkerbell/tink/pull/549
-# see https://github.com/tinkerbell/tink/pull/552
+# see https://github.com/tinkerbell/tink/pull/549 tink-worker: do not attach the action container stdout/stderr to the current process when --capture-action-logs=false
+# see https://github.com/tinkerbell/tink/pull/552 Upgrade to PostgreSQL 14
 cd ~/tinkerbell-tink
 go install golang.org/x/tools/cmd/goimports
 go install golang.org/x/tools/cmd/stringer
@@ -28,4 +28,4 @@ docker buildx build \
 docker manifest inspect $TINKERBELL_HOST_IP/tink-worker
 
 # ensure tink-worker will not be re-installed by compose.
-sed -i -E 's,^([^#]+ tink-worker:latest),#\1,g' ~/tinkerbell-sandbox/deploy/compose/registry/registry_images.txt
+sed -i -E 's,^(quay\.io/tinkerbell/tink-worker:.+),#\1,g' ~/tinkerbell-sandbox/deploy/compose/registry/registry_images.txt
